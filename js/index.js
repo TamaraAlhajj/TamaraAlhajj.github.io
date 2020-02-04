7ccc10bdb69730be2018dfb0135fe15e7d95166b
TamaraAlhajj <tamara.alhajj@carleton.ca>
2020-02-03 21:44:05 -0500

    added preloader

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// $(window).on("load",function(){
+//     $(".loader-wrapper").fadeOut("slow");
+// });
+
+window.addEventListener('load', () => {
+    const preload = document.querySelector('.loader-wrapper');
+    preload.classList.add('preloadFinish');
+})
