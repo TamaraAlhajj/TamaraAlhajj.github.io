bc4d4c0aee66c132fdf17b0088c538ceb03f7c32
TamaraAlhajj <tamaraalhajjcs@gmail.com>
2023-12-02 22:42:29 -0500

    update resume and stylings

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // preloader functionality
 $(document).ready(function () {
-    $('.loader-wrapper').fadeOut(3000);
+    $('.loader-wrapper').fadeOut(1700);
     $('.heading-primary--main').addClass('animate--main');
     $('.heading-primary--sub').addClass('animate--sub'); 
 });
